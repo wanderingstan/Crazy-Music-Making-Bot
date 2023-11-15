2d5433d7a7d1cde7fd914a8244b4968144f0fb37
Stan James <stan@wanderingstan.com>
2023-11-15 16:19:31 -0700

    Working with slash commands And cleaned up a lot.

--- main.py
+++ main.py
@@ -1,192 +1,136 @@
-import discord
-from discord.ext import commands
-
+from interactions import Client, Intents, listen, File
+from interactions import slash_command, SlashContext
+from interactions import OptionType, slash_option
 import replicate
 import aiohttp
 import os
 import logging
 import asyncio
-from sound_generation import generate_and_download_music
 from music_generation import music_generation
 from video_generation import generate_video
 from glif import call_glif_api
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
 
 # Environment Variables
-TOKEN = os.getenv('mydiscordtoken')
-REPLICATE_API_TOKEN = os.getenv('REPLICATE_API_TOKEN')
-ACTIVE_CHANNEL_ID = os.getenv('ACTIVE_CHANNEL_ID')
+TOKEN = os.getenv("DISCORD_TOKEN")
+REPLICATE_API_TOKEN = os.getenv("REPLICATE_API_TOKEN")
+ACTIVE_CHANNEL_ID = os.getenv("ACTIVE_CHANNEL_ID")
 TEMP_PATH = "temp_files/"
-# ACTIVE_CHANNEL_ID = "988352005569384518"  # Replace with your channel ID
 
-# Discord Bot Intents
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='/', intents=intents)
+# Test if we have valid keys
+# Check if TOKEN is valid
+if not TOKEN:
+    raise ValueError("DISCORD_TOKEN environment variable is not set or empty.")
+
+# Check if REPLICATE_API_TOKEN is valid
+if not REPLICATE_API_TOKEN:
+    raise ValueError("REPLICATE_API_TOKEN environment variable is not set or empty.")
+
+# Check if ACTIVE_CHANNEL_ID is valid
+if not ACTIVE_CHANNEL_ID:
+    raise ValueError("ACTIVE_CHANNEL_ID environment variable is not set or empty.")
+
+if not os.path.exists(TEMP_PATH):
+    raise ValueError("TEMP_PATH variable points to non existant directory.")
+
+# Discord Bot
+bot = Client(intents=Intents.DEFAULT)
 
 # Replicate Client
 replicate_client = replicate.Client(api_token=REPLICATE_API_TOKEN)
 
 
-# async def generate_music(prompt,
-#                          duration="5.0",
-#                          guidance_scale=2.5,
-#                          n_candidates=3):
-#   fullprompt = f"8 bit nintendo style gaming music with {prompt}"
-
-#   output = replicate_client.run(
-#     "haoheliu/audio-ldm:b61392adecdd660326fc9cfc5398182437dbe5e97b5decfb36e1a36de68b5b95",
-#     input={
-#       "text": fullprompt,
-#       "duration": duration,
-#       "guidance_scale": guidance_scale,
-#       "n_candidates": n_candidates
-#     })
-
-#   if output is None:
-#     raise Exception("No output was returned from the model.")
-
-#   # Assuming the model returns a URL
-#   return output
-async def download_music(url: str) -> str:
-  logging.info("Starting download of music.")
-  async with aiohttp.ClientSession() as session:
-    async with session.get(url) as response:
-      if response.status == 200:
-        music_filename = 'temp_music.mp3'
-        with open(music_filename, 'wb') as f:
-          f.write(await response.read())
-        logging.info("Music downloaded successfully.")
-        return music_filename
-      else:
-        logging.error(f"Error downloading music: {response.status}")
-        return ''
-
-
-# @bot.command()
-# async def inspo(ctx, *, text: str):
-#   if str(ctx.channel.id) != ACTIVE_CHANNEL_ID:
-#     logging.info(f"Ignoring channel {ctx.channel.id}")
-#     return
-
-#   prefix = TEMP_PATH + str(ctx.message.id) + "_music"
-
-#   try:
-#     mp3_path = await generate_and_download_music(text, prefix)
-#     if mp3_path:
-#       await ctx.send(file=discord.File(mp3_path))
-
-#       os.remove(mp3_path)
-#     else:
-#       await ctx.send("An error occurred while downloading the music.")
-#   except Exception as e:
-#     logging.exception("An error occurred while handling the request.")
-#     await ctx.send(f"An error occurred while handling your request: {e}")
-
-
-@bot.command(name='music', description='Generate some music from text')
+@slash_command(
+    name="music",
+    description="Generate some music from text",
+    scopes=[ACTIVE_CHANNEL_ID],
+)
 async def music(ctx, *, text: str = ""):
-  if str(ctx.channel.id) != ACTIVE_CHANNEL_ID:
-    logging.info(f"Ignoring channel {ctx.channel.id}")
-    return
+    try:
+        logging.info("music_generation")
+        mp3_path = await music_generation(text)
+        if mp3_path:
+            async with ctx.typing():
+                await ctx.send(files=File(mp3_path))
+                os.remove(mp3_path)
+        else:
+            await ctx.send("An error occurred while generating the music.")
 
-  if len(text.strip()) == 0:
-    await ctx.send("You didn't give a style of music.")
-    return
-
-  # await ctx.send(f"Composing some music in the theme of {text}...")
-
-  # Add a checkbox reaction to the message that triggered the command
-  await ctx.message.add_reaction('🕑')
-
-  # async with ctx.typing():
-
-  try:
-    logging.info("music_generation")
-    mp3_path = await music_generation(text)
-    if mp3_path:
-      async with ctx.typing():
-        await ctx.send(file=discord.File(mp3_path))
-        os.remove(mp3_path)
-    else:
-      await ctx.send("An error occurred while generating the music.")
-
-  except Exception as e:
-    logging.exception("An error occurred while handling the music request.")
-    await ctx.send(f"An error occurred while handling your music request: {e}")
-
-  # Get the bot's user object
-  bot_user = ctx.guild.get_member(bot.user.id)
-  # Remove the checkbox reaction from the message
-  await ctx.message.remove_reaction('🕑', bot_user)
+    except Exception as e:
+        logging.exception("An error occurred while handling the music request.")
+        await ctx.send(f"An error occurred while handling your music request: {e}")
 
 
-@bot.command(name='image', description='Generate an image from text')
+@slash_command(
+    name="image",
+    description="Generate an image from text",
+    scopes=[ACTIVE_CHANNEL_ID],
+)
 async def image(ctx, *, text: str):
-  if str(ctx.channel.id) != ACTIVE_CHANNEL_ID:
-    logging.info(f"Ignoring channel {ctx.channel.id}")
-    return
-
-  try:
-    image_url = await call_glif_api(text)
-    if image_url:
-      await ctx.send(image_url)  # This will send the image URL directly
-    else:
-      await ctx.send("An error occurred or no image URL was returned.")
-  except Exception as e:
-    logging.exception("An error occurred while handling the image request.")
-    await ctx.send(f"An error occurred while handling your image request: {e}")
+    try:
+        image_url = await call_glif_api(text)
+        if image_url:
+            await ctx.send(image_url)  # This will send the image URL directly
+        else:
+            await ctx.send("An error occurred or no image URL was returned.")
+    except Exception as e:
+        logging.exception("An error occurred while handling the image request.")
+        await ctx.send(f"An error occurred while handling your image request: {e}")
 
 
-@bot.command(name='video')
-async def video(ctx, *, text: str):
-  if str(ctx.channel.id) != ACTIVE_CHANNEL_ID:
-    logging.info(f"Ignoring channel {ctx.channel.id}")
-    return
+@slash_command(
+    name="video",
+    description="Generate a video from text",
+    scopes=[ACTIVE_CHANNEL_ID],
+)
+@slash_option(
+    name="prompt",
+    description="Describe the type of retro game you want a video for.",
+    required=True,
+    opt_type=OptionType.STRING,
+)
+async def video(ctx, *, prompt: str):
+    await ctx.defer()
 
-  # Add a checkbox reaction to the message that triggered the command
-  await ctx.message.add_reaction('🕑')
+    try:
+        # Start both glif API and replicate API calls concurrently
+        image_path, mp3_path = await asyncio.gather(
+            call_glif_api(prompt),  # Ensure this returns a local file path
+            music_generation(
+                prompt, filename_prefix=TEMP_PATH
+            ),  # Ensure this returns a local file path
+        )
 
-  try:
-    async with ctx.typing():
-      # Start both glif API and replicate API calls concurrently
-      image_path, mp3_path = await asyncio.gather(
-        call_glif_api(text),  # Ensure this returns a local file path
-        music_generation(
-          text,
-          filename_prefix=TEMP_PATH)  # Ensure this returns a local file path
-      )
+        # Check if both the image and the music were successfully generated
+        if image_path and mp3_path:
+            # Generate the video
+            video_path = await generate_video(image_path, mp3_path, TEMP_PATH)
 
-    # Check if both the image and the music were successfully generated
-    if image_path and mp3_path:
-      # Generate the video
-      video_path = await generate_video(image_path, mp3_path, TEMP_PATH)
+            # Send the video to the Discord channel
+            await ctx.send(file=File(video_path))
 
-      # Send the video to the Discord channel
-      await ctx.send(file=discord.File(video_path))
+            # Clean up the generated files
+            os.path.exists(video_path) and os.remove(video_path)
+            os.path.exists(mp3_path) and os.remove(mp3_path)
+            os.path.exists(image_path) and os.remove(image_path)
+        else:
+            await ctx.send("An error occurred while generating the video components.")
 
-      # Clean up the generated files
-      os.path.exists(video_path) and os.remove(video_path)
-      os.path.exists(mp3_path) and os.remove(mp3_path)
-      os.path.exists(image_path) and os.remove(image_path)
-    else:
-      await ctx.send("An error occurred while generating the video components."
-                     )
-
-  except Exception as e:
-    logging.exception("An error occurred while handling the video request.")
-    await ctx.send(f"An error occurred while handling your video request: {e}")
-
-  # Get the bot's user object
-  bot_user = ctx.guild.get_member(bot.user.id)
-  # Remove the checkbox reaction from the message
-  await ctx.message.remove_reaction('🕑', bot_user)
+    except Exception as e:
+        logging.exception(f"An error occurred while handling the video request: {e}")
+        await ctx.send(f"An error occurred while handling your video request: {e}")
 
 
-@bot.event
+@listen()  # this decorator tells snek that it needs to listen for the corresponding event, and run this coroutine
 async def on_ready():
-  logging.info(f"🔵 {bot.user.name} is now running!")
+    # This event is called when the bot is ready to respond to commands
+    logging.info("Ready")
+    logging.info(f"This bot is owned by {bot.owner}")
 
 
-bot.run(TOKEN)
+bot.start(TOKEN)
--- video_generation.py
+++ video_generation.py
@@ -5,82 +5,90 @@ import logging
 import random
 
 # Configure logging
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 # Default image URL
-DEFAULT_IMAGE_URL = 'https://glif.app/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdzkwltgyd%2Fimage%2Fupload%2Fv1699551574%2Fglif-run-outputs%2Fs6s7h7fypr9pr35bpxul.png&w=2048&q=75&dpl=dpl_J7MPoF6chivDp2KiVJA6mJ9faKu1'
+DEFAULT_IMAGE_URL = "https://glif.app/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdzkwltgyd%2Fimage%2Fupload%2Fv1699551574%2Fglif-run-outputs%2Fs6s7h7fypr9pr35bpxul.png&w=2048&q=75&dpl=dpl_J7MPoF6chivDp2KiVJA6mJ9faKu1"
 
 
 async def download_file(url, file_name):
-  async with aiohttp.ClientSession() as session:
-    async with session.get(url) as response:
-      if response.status == 200:
-        with open(file_name, 'wb') as f:
-          while True:
-            chunk = await response.content.read(1024)
-            if not chunk:
-              break
-            f.write(chunk)
-        logging.info(f"File downloaded successfully: {file_name}")
-        return file_name
-      else:
-        error_msg = f"Failed to download file from {url}"
-        logging.error(error_msg)
-        raise Exception(error_msg)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                with open(file_name, "wb") as f:
+                    while True:
+                        chunk = await response.content.read(1024)
+                        if not chunk:
+                            break
+                        f.write(chunk)
+                logging.info(f"File downloaded successfully: {file_name}")
+                return file_name
+            else:
+                error_msg = f"Failed to download file from {url}"
+                logging.error(error_msg)
+                raise Exception(error_msg)
 
 
-async def generate_video(image_source: str = None,
-                         audio_source: str = None,
-                         file_prefix: str = './') -> str:
-  # Generate random identifier for file naming
-  random_id = random.randint(1, 99999999)
+async def generate_video(
+    image_source: str = None, audio_source: str = None, file_prefix: str = "./"
+) -> str:
+    logging.info("generate_video")
 
-  # Define the paths for temporary files
-  image_path = f'{file_prefix}img{random_id}.jpg'
-  audio_path = f'{file_prefix}wav{random_id}.mp3'
-  video_path = f'{file_prefix}video{random_id}.mp4'
+    # Generate random identifier for file naming
+    random_id = random.randint(1, 99999999)
 
-  # Download or assign image file
-  if image_source and (image_source.startswith('http://')
-                       or image_source.startswith('https://')):
-    image_path = await download_file(image_source, image_path)
-  elif image_source:
-    image_path = image_source
-  else:
-    image_path = await download_file(DEFAULT_IMAGE_URL, image_path)
+    # Define the paths for temporary files
+    image_path = f"{file_prefix}img{random_id}.jpg"
+    audio_path = f"{file_prefix}wav{random_id}.mp3"
+    video_path = f"{file_prefix}video{random_id}.mp4"
 
-  # Download or assign audio file
-  if audio_source and (audio_source.startswith('http://')
-                       or audio_source.startswith('https://')):
-    audio_path = await download_file(audio_source, audio_path)
-  else:
-    audio_path = audio_source
+    # Download or assign image file
+    if image_source and (
+        image_source.startswith("http://") or image_source.startswith("https://")
+    ):
+        image_path = await download_file(image_source, image_path)
+    elif image_source:
+        image_path = image_source
+    else:
+        image_path = await download_file(DEFAULT_IMAGE_URL, image_path)
 
-  # Construct the ffmpeg command with fixed 8 seconds duration
-  cmd = (f"ffmpeg -loop 1 -framerate 2 -i {image_path} -i {audio_path} "
-         f"-c:v libx264 -tune stillimage -c:a aac -strict experimental "
-         f"-b:a 192k -pix_fmt yuv420p -t 8 {video_path}")
+    # Download or assign audio file
+    if audio_source and (
+        audio_source.startswith("http://") or audio_source.startswith("https://")
+    ):
+        audio_path = await download_file(audio_source, audio_path)
+    else:
+        audio_path = audio_source
 
-  # Run the ffmpeg command
-  process = await asyncio.create_subprocess_shell(
-    cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    # Construct the ffmpeg command with fixed 8 seconds duration
+    cmd = (
+        f"ffmpeg -loop 1 -framerate 2 -i {image_path} -i {audio_path} "
+        f"-c:v libx264 -tune stillimage -c:a aac -strict experimental "
+        f"-b:a 192k -pix_fmt yuv420p -t 8 {video_path}"
+    )
 
-  # Wait for the command to complete
-  await process.communicate()
+    # Run the ffmpeg command
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
 
-  if process.returncode != 0:
-    error_message = f"ffmpeg command failed for video{random_id}"
-    logging.error(error_message)
-    raise Exception(error_message)
+    # Wait for the command to complete
+    await process.communicate()
 
-  logging.info(f"Video generated successfully: {video_path}")
+    if process.returncode != 0:
+        error_message = f"ffmpeg command failed for video{random_id}"
+        logging.error(error_message)
+        raise Exception(error_message)
 
-  # Delete temp files
-  os.path.exists(image_path) and os.remove(image_path)
-  os.path.exists(audio_path) and os.remove(audio_path)
+    logging.info(f"Video generated successfully: {video_path}")
 
-  return video_path
+    # Delete temp files
+    os.path.exists(image_path) and os.remove(image_path)
+    os.path.exists(audio_path) and os.remove(audio_path)
+
+    return video_path
 
 
 # Example usage:
