8d9b96ae69d8f98de1b6fda9acc950e43536df41
Stan James <stan@wanderingstan.com>
2023-11-29 10:43:53 -0700

    speech + crappy video generation working

--- film4.py
+++ film4.py
@@ -1,8 +1,10 @@
+from typing import List
 import logging
 import config
 import asyncio
 import replicate
 import aiohttp
+import shutil
 import json
 import os
 import video_generation
@@ -16,20 +18,37 @@ class Film4:
         self.data = json.loads(json_input)
         self.filename_prefix = filename_prefix
 
-    def generate(self):
-        # Parse the JSON input
+    async def generate(self):
+        # Assuming self.data is already set
 
-        # Iterate through each scene in the 'scenes' array
-        for scene in self.data["scenes"]:
-            # Extract information from the scene and call create_scene
-            asyncio.run(
-                self.create_scene(
-                    scene["duration"],
-                    scene["video_prompt"],
-                    scene["sound_prompt"],
-                    scene["text"],
-                )
+        narration = self.data["narration"]
+        speech_filename = f"{self.filename_prefix}narration.mp3"
+        await self.speech_from_text(narration, speech_filename)
+
+        # Collect tasks for each scene
+        tasks = [
+            self.create_scene(
+                scene["duration"],
+                scene["video_prompt"],
+                scene["sound_prompt"],
+                scene["text"],
             )
+            for scene in self.data["scenes"]
+        ]
+
+        # Run all scene creation tasks concurrently and collect results
+        video_filenames = await asyncio.gather(*tasks)
+
+        combined_video_filename = await self.concatenate_videos_with_audio(
+            video_filenames,
+            speech_filename,
+            # "./test_files/wanderingstan_1175179192011333713_music.wav",
+            "output.mp4",
+        )
+
+        logging.info(f"🟢 Generated video: {combined_video_filename}")
+
+        return combined_video_filename
 
     async def create_scene(self, duration, video_prompt, sound_prompt, text):
         # Implement your scene creation logic here
@@ -38,19 +57,21 @@ class Film4:
         )
 
         # Start both glif API and replicate API calls concurrently
-        video_url, sound_url = await asyncio.gather(
-            self.video_from_prompt(video_prompt),
-            self.sound_from_prompt(sound_prompt),
+        video_path, sound_url = await asyncio.gather(
+            self.video_from_prompt(video_prompt, duration),
+            self.sound_from_prompt(sound_prompt, duration),
         )
 
-        # GENERATE VIDEO
+        # TODO: use the sound
 
-    async def sound_from_prompt(self, prompt: str) -> str:
+        return video_path
+
+    async def sound_from_prompt(self, prompt: str, duration: int = 4) -> str:
         # Implement sound_from_prompt logic
         return None
 
-    async def video_from_prompt(self, prompt: str) -> str:
-        """ Generate video from prompt using Replicate API, return path to video file."""
+    async def video_from_prompt(self, prompt: str, duration: int = 4) -> str:
+        """Generate video from prompt using Replicate API, return URL to video file."""
 
         logging.info(f"🎬 Generating video for prompt: {prompt}")
 
@@ -67,47 +88,179 @@ class Film4:
 
         logging.info("🕒 Calling the Replicate API for video generation.")
 
-        # Run the new music generation model using Replicate API
-        # https://replicate.com/cjwbw/kandinskyvideo?input=python
+        # # Run the video generation model using Replicate API
+
+        # # https://replicate.com/cjwbw/kandinskyvideo?input=python
+        # output_url = await asyncio.to_thread(
+        #     replicate_client.run,
+        #     "cjwbw/kandinskyvideo:849b70f3e300a650aa8b78d0f8f24d104824b832ea7f61c79bd2c7e78a4ad545",
+        #     input={
+        #         "fps": 10,
+        #         "width": 640,
+        #         "height": 384,
+        #         "prompt": prompt,
+        #         "guidance_scale": 5,
+        #         "interpolation_level": "low",
+        #         "num_inference_steps": 50,
+        #         "interpolation_guidance_scale": 0.25,
+        #     },
+        # )
+
+        # https://replicate.com/cjwbw/text2video-zero?input=python
         output_url = await asyncio.to_thread(
             replicate_client.run,
-            "cjwbw/kandinskyvideo:849b70f3e300a650aa8b78d0f8f24d104824b832ea7f61c79bd2c7e78a4ad545",
+            "cjwbw/text2video-zero:e671ffe4e976c0ec813f15a9836ebcfd08857ac2669af6917e3c2549307f9fae",
             input={
-                "fps": 10,
-                "width": 640,
-                "height": 384,
+                "fps": 4,
                 "prompt": prompt,
-                "guidance_scale": 5,
-                "interpolation_level": "low",
-                "num_inference_steps": 50,
-                "interpolation_guidance_scale": 0.25,
+                "model_name": "dreamlike-art/dreamlike-photoreal-2.0",
+                "timestep_t0": 44,
+                "timestep_t1": 47,
+                "video_length": duration,
+                "negative_prompt": "",
+                "motion_field_strength_x": 12,
+                "motion_field_strength_y": 12,
             },
         )
 
         if output_url is None:
             raise Exception("No output_URL was returned from the model.")
 
-        # Download the generated video file
-        output_format = "mp4"
-        filename = f"{filename_prefix}{sanitize_for_unix_filename(prompt)}.{output_format}"
-        filename = f"{filename_prefix}video.{output_format}"
+        return output_url
 
-        logging.info(f"🟢 Saving video to {filename}")
+        # # Download the generated video file
+        # output_format = "mp4"
+        # filename = f"{self.filename_prefix}video_{self.sanitize_for_unix_filename(prompt)}.{output_format}"
+
+        # logging.info(f"🟢 Saving video to {filename}")
+
+        # async with aiohttp.ClientSession() as session:
+        #     async with session.get(output_url) as response:
+        #         if response.status == 200:
+        #             with open(filename, "wb") as f:
+        #                 f.write(await response.read())
+        #             return filename
+        #         else:
+        #             raise Exception(f"Error downloading video: {response.status}")
+
+    async def speech_from_text(
+        self,
+        text: str,
+        output_filename: str,
+        voice_model_id: str = "21m00Tcm4TlvDq8ikWAM",
+    ):
+        """ " Generate speech from text using Eleven Labs API, return path to mp3 audio file."""
+        url = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_model_id}"
+        headers = {
+            "Content-Type": "application/json",
+            "xi-api-key": config.ELEVENLABS_API_TOKEN,
+        }
+        data = {"text": text}
 
         async with aiohttp.ClientSession() as session:
-            async with session.get(output_url) as response:
+            async with session.post(url, headers=headers, json=data) as response:
                 if response.status == 200:
-                    with open(filename, "wb") as f:
+                    with open(output_filename, "wb") as f:
                         f.write(await response.read())
-                    return filename
+                    logging.info(f"🟢 Speech audio saved as {output_filename}")
                 else:
-                    raise Exception(f"Error downloading music: {response.status}")
+                    logging.error(f"Request failed: {response.status}")
+                    text = await response.text()
+                    logging.error(text)
+
+    async def concatenate_videos_with_audio(
+        self, video_files: List[str], audio_file: str, output_file: str = "output.mp4"
+    ):
+        # Ensure FFmpeg is installed
+        if not shutil.which("ffmpeg"):
+            raise RuntimeError("FFmpeg is not installed or not in the PATH.")
+
+        # Build the FFmpeg command
+        input_cmd = " ".join(f"-i {video}" for video in video_files)
+        filter_complex = " ".join(f"[{i}:v]" for i in range(len(video_files)))
+        n_videos = len(video_files)
+
+        cmd = (
+            f"ffmpeg {input_cmd} -i {audio_file} "
+            f'-filter_complex "{filter_complex}concat=n={n_videos}:v=1:a=0[outv]" '
+            f'-map "[outv]" -map {n_videos}:a -c:a aac -strict -2 -y {output_file}'
+        )
+
+        # Run the command asynchronously
+        process = await asyncio.create_subprocess_shell(
+            cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+        )
+
+        # Wait for the command to complete and capture stdout and stderr
+        stdout, stderr = await process.communicate()
+
+        # Check for errors
+        if process.returncode != 0:
+            error_message = f"FFmpeg command failed: {stderr.decode('utf-8')}"
+            raise Exception(error_message)
+
+        return os.path.abspath(output_file)
 
 
-        
+
+# [vost#0:0/copy @ 0x124b1ee50] Streamcopy requested for output stream fed from a complex filtergraph. Filtering and streamcopy cannot be used together.
+
+    # async def concatenate_videos_with_audio(
+    #     self,
+    #     video_files: List[str],
+    #     audio_files: List[str],
+    #     output_file: str = "output.mp4",
+    # ):
+    #     # Ensure FFmpeg is installed
+    #     if not shutil.which("ffmpeg"):
+    #         raise RuntimeError("FFmpeg is not installed or not in the PATH.")
+
+    #     # Build the FFmpeg command for video inputs
+    #     video_input_cmd = " ".join(f"-i {video}" for video in video_files)
+    #     video_filter_complex = " ".join(f"[{i}:v]" for i in range(len(video_files)))
+    #     n_videos = len(video_files)
+
+    #     # Build the FFmpeg command for audio inputs
+    #     audio_input_cmd = " ".join(f"-i {audio}" for audio in audio_files)
+    #     audio_filter_complex = " ".join(
+    #         f"[{n_videos + i}:a]" for i in range(len(audio_files))
+    #     )
+    #     n_audios = len(audio_files)
+
+    #     # Create complex filter to mix audio files and concatenate video files
+    #     filter_complex = (
+    #         f"{video_filter_complex}concat=n={n_videos}:v=1:a=0[outv]; "
+    #         f"{audio_filter_complex}amix=inputs={n_audios}[outa]"
+    #     )
+
+    #     cmd = (
+    #         f"ffmpeg {video_input_cmd} {audio_input_cmd} "
+    #         f'-filter_complex "{filter_complex}" '
+    #         f'-map "[outv]" -map "[outa]" -c:v copy -c:a aac -strict -2 -y {output_file}'
+    #     )
+
+    #     # Run the command asynchronously
+    #     process = await asyncio.create_subprocess_shell(
+    #         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    #     )
+
+    #     # Wait for the command to complete and capture stdout and stderr
+    #     stdout, stderr = await process.communicate()
+
+    #     # Check for errors
+    #     if process.returncode != 0:
+    #         error_message = f"FFmpeg command failed: {stderr.decode('utf-8')}"
+    #         raise Exception(error_message)
+
+    #     return os.path.abspath(output_file)
 
     def sanitize_for_unix_filename(self, filename: str) -> str:
-        return "".join(char for char in filename if char.isalnum() or char in "._-")
+        """Helper function to sanitize string to valid filename, max len 100"""
+        return "".join(
+            char
+            for char in filename.replace(" ", "_")
+            if char.isalnum() or char in "_-"
+        )[:100]
 
 
 # Test code to run when script is executed directly
@@ -115,15 +268,16 @@ if __name__ == "__main__":
     example_json_input = """{
         "duration": "30",
         "music_prompt": "orchestral EDM with a hint of bluegrass, rising to a crescendo",
+        "narration" : "Bunnies should be very very careful these days. They are being hunted by the evil foxes.",
         "scenes": [
             {
-                "duration": 10,
+                "duration": 2,
                 "video_prompt": "a colorful rabbit juming across a green field",
                 "sound_prompt": "crickets chirping",
                 "text": "Funny bunny is happy"
             },
             {
-                "duration": 15,
+                "duration": 4,
                 "video_prompt": "a colorful rabbit lying dead in the street",
                 "sound_prompt": "machine gun firing",
                 "text": "Funny bunny is not happy"
@@ -132,168 +286,15 @@ if __name__ == "__main__":
     }"""
 
     film4 = Film4(example_json_input)
-    film4.generate()
 
-    # test_prompt = "a cyclist riding fast through a corner on a california road, close view, sunny day"
-    # loop = asyncio.get_event_loop()
-    # try:
-    #     video_file = loop.run_until_complete(
-    #         film4.video_from_prompt(test_prompt, "./temp_files/test_")
-    #     )
-    #     print(f"Generated video saved to: {video_file}")
-    # except Exception as e:
-    #     print(f"Error occurred: {e}")
-    # finally:
-    #     loop.close()
+    # Run the generate method in an asyncio event loop
+    loop = asyncio.get_event_loop()
+    try:
+        combined_video_filename = loop.run_until_complete(film4.generate())
+        print(f"Generated combined video: {combined_video_filename}")
+    except Exception as e:
+        print(f"Error occurred: {e}")
+    finally:
+        loop.close()
 
-
-# def process_json_input(json_input):
-#     # example_json_input = """{
-#     #     "duration": "30",
-#     #     "music_prompt": "orchestral EDM with a hint of bluegrass, rising to a crescendo",
-#     #     "scenes": [
-#     #         {
-#     #             "duration": 10,
-#     #             "video_prompt": "a colorful rabbit juming across a green field",
-#     #             "sound_prompt": "crickets chirping",
-#     #             "text": "Funny bunny is happy"
-#     #         },
-#     #         {
-#     #             "duration": 15,
-#     #             "video_prompt": "a colorful rabbit lying dead in the street",
-#     #             "sound_prompt": "machine gun firing",
-#     #             "text": "Funny bunny is not happy"
-#     #         }
-#     #     ]
-#     # }"""
-
-#     # Parse the JSON input
-#     data = json.loads(json_input)
-
-#     # Iterate through each scene in the 'scenes' array
-#     for scene in data["scenes"]:
-#         # Extract information from the scene
-#         duration = scene["duration"]
-#         video_prompt = scene["video_prompt"]
-#         sound_prompt = scene["sound_prompt"]
-#         text = scene["text"]
-
-#         # Call the create_scene function
-#         create_scene(duration, video_prompt, sound_prompt, text)
-
-
-# async def create_scene(duration, video_prompt, sound_prompt, text):
-#     # Implement your scene creation logic here
-#     print(f"Creating scene with duration {duration} and video prompt {video_prompt}")
-
-#     try:
-#         # Start both glif API and replicate API calls concurrently
-#         video_url, sound_url = await asyncio.gather(
-#             video_from_prompt(video_prompt),
-#             sound_from_prompt(sound_prompt),
-#         )
-
-#         # Check if both the image and the music were successfully generated
-#         if video_from_prompt and sound_from_prompt:
-#             # Generate the video
-#             video_path = await video_generation.generate_video(
-#                 image_path, mp3_path, temp_file_prefix(ctx), duration=8
-#             )
-
-#             # Send the video to the Discord channel
-#             await ctx.send(file=File(video_path))
-#             await ctx.send(prompt)
-
-#             # Clean up the generated files
-#             if config.DELETE_TEMP_FILES:
-#                 os.path.exists(video_path) and os.remove(video_path)
-#                 os.path.exists(mp3_path) and os.remove(mp3_path)
-#                 os.path.exists(image_path) and os.remove(image_path)
-#         else:
-#             await ctx.send("An error occurred while generating the video components.")
-
-#     except Exception as e:
-#         logging.exception(f"An error occurred while handling the video request: {e}")
-#         await ctx.send(f"An error occurred while handling your video request: {e}")
-
-
-# # Function to call XXXX API, return URL to sound
-# async def sound_from_prompt(prompt: str, filename_prefix: str) -> str:
-#     return None
-
-
-# # Function to call Replicate API, return URL to video
-# async def video_from_prompt(prompt: str, filename_prefix: str) -> str:
-#     logging.info(f"🎬 Generating video for prompt: {prompt}")
-
-#     # Testing
-#     if config.DO_FAKE_RESULTS:
-#         logging.info("😎 Using fake replicate video response for testing.")
-#         return "test_files/video_gen_sample.mp4"
-
-#     if not config.REPLICATE_API_TOKEN:
-#         raise ValueError("No Replicate API token REPLICATE_API_TOKEN.")
-
-#     # Initialize Replicate Client with your API token
-#     replicate_client = replicate.Client(api_token=config.REPLICATE_API_TOKEN)
-
-#     logging.info("🕒 Calling the Replicate API for video generation.")
-
-#     # Run the new music generation model using Replicate API
-#     # https://replicate.com/cjwbw/kandinskyvideo?input=python
-#     output_url = await asyncio.to_thread(
-#         replicate_client.run,
-#         "cjwbw/kandinskyvideo:849b70f3e300a650aa8b78d0f8f24d104824b832ea7f61c79bd2c7e78a4ad545",
-#         input={
-#             "fps": 10,
-#             "width": 640,
-#             "height": 384,
-#             "prompt": prompt,
-#             "guidance_scale": 5,
-#             "interpolation_level": "low",
-#             "num_inference_steps": 50,
-#             "interpolation_guidance_scale": 0.25,
-#         },
-#     )
-
-#     if output_url is None:
-#         raise Exception("No output_URL was returned from the model.")
-
-#     # Download the generated music file
-#     output_format = "mp4"
-#     filename = f"{filename_prefix}{sanitize_for_unix_filename(prompt)}.{output_format}"
-#     filename = f"{filename_prefix}video.{output_format}"
-
-#     logging.info(f"🟢 Saving video to {filename}")
-
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get(output_url) as response:
-#             if response.status == 200:
-#                 with open(filename, "wb") as f:
-#                     f.write(await response.read())
-#                 return filename
-#             else:
-#                 raise Exception(f"Error downloading music: {response.status}")
-
-
-# # Function to sanitize filename (assuming you need it)
-# def sanitize_for_unix_filename(filename: str) -> str:
-#     return "".join(char for char in filename if char.isalnum() or char in "._-")
-
-
-# # Entry point for direct CLI execution
-# if __name__ == "__main__":
-#     # Test prompt for video generation
-#     test_prompt = "a cyclist riding fast through a corner on a california road, close view, sunny day"
-
-#     # Run the video generation in an asyncio event loop
-#     loop = asyncio.get_event_loop()
-#     try:
-#         video_file = loop.run_until_complete(
-#             video_from_prompt(test_prompt, "./temp_files/test_")
-#         )
-#         print(f"Generated video saved to: {video_file}")
-#     except Exception as e:
-#         print(f"Error occurred: {e}")
-#     finally:
-#         loop.close()
+    # asyncio.run(film4.speech_from_text("Test of voice", "./temp_files/test_voice.mp3"))
